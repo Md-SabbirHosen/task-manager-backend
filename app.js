61773bdd197d836dfc050d80268f996cc8991bd0
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-04-21 22:18:22 +0600

    commited

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("task manager backend");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the home page!");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000!");
+});
